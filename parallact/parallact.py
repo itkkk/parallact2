44ba286e81f56b1c0a9d5dfcaba1cfc9d37af9e9
Anthony Pellicani <anthony.pellicani@gmail.com>
2021-05-07 12:49:17 +0200

    Completed parallact module

--- parallact/parallact.py
+++ parallact/parallact.py
@@ -3,26 +3,61 @@ import statistics
 from datetime import datetime
 
 
-def load_generic_dataset(path, chain_len=3, verbose=False, save_to_disk=False, filename="", time_format=""):
+def load_generic_dataset(path, window_size=3, verbose=False, save_to_disk=False, filename="", time_format=""):
     """
-    :param path:
-    :param chain_len:
-    :param verbose:
-    :param save_to_disk:
-    :param filename:
-    :param time_format:
-    :return:
+    This function loads a generic dataset, and exploits chain_dataset function in order to apply the sliding window
+    to each trace.
+    The dataset must have at least 3 columns in this order: CaseID, ActivityID, CompleteTimestamp
+
+    :param path: the dataset path
+    :param window_size: the sliding window size
+    :param verbose: if True show average trace length and standard deviation. DEFAULT = False
+    :param save_to_disk: if True the preprocessed dataset will be saved to disk. DEFAULT = False
+    :param filename: the name of the file that will be written to disk
+    :param time_format: the timestamp format, usually "%Y-%m-%d %H:%M:%S"
+    :return: a pandas dataframe containing the preprocessed dataset.
+             This dataframe contains 5 columns:
+             "caseID": the case id
+             "activity_chain": a partial trace with length window_size
+             "next_activity": the activity executed after the last one in the partial trace
+             "global_timestamps": the normalized difference between the timestamp of each activity in the partial trace
+                                  and the timestamp of the first activity in the original trace
+             "local_timestamps": the normalized difference between the timestamp of each activity in the partial trace
+                                 and the timestamp of the previous one
     """
     dataset = pd.read_csv(path, index_col=0)
     pd.set_option('display.max_columns', None)
 
-    return chain_dataset(dataset, chain_len, verbose, save_to_disk, filename, time_format)
+    return chain_dataset(dataset, window_size, verbose, save_to_disk, filename, time_format)
 
 
-def chain_dataset(dataset, chain_len, verbose, save_to_disk, filename, time_format):
+def chain_dataset(dataset, window_size, verbose, save_to_disk, filename, time_format):
+    """
+    This function is used to preprocess a dataset, applying the sliding window.
+    NB: if you want to load a dataset which is stored in a three column csv, use "load_generic_dataset" function,
+        otherwise if your file has a different structure write your own "load_dataset" in order to create
+        a three columns pandas dataframe which has to be passed to this function.
+
+    :param dataset: a pandas dataframe containg a dataset with at least 3 columns in this order:
+                    CaseID, ActivityID, CompleteTimestamp
+    :param window_size: the sliding window size
+    :param verbose: if True show average trace length and standard deviation
+    :param save_to_disk: if True the preprocessed dataset will be saved to disk.
+    :param filename: the name of the file that will be written to disk
+    :param time_format: the timestamp format, usually "%Y-%m-%d %H:%M:%S"
+    :return: a pandas dataframe containing the preprocessed dataset.
+             This dataframe contains 5 columns:
+             "caseID": the case id
+             "activity_chain": a partial trace with length window_size
+             "next_activity": the activity executed after the last one in the partial trace
+             "global_timestamps": the normalized difference between the timestamp of each activity in the partial trace
+                                  and the timestamp of the first activity in the original trace
+             "local_timestamps": the normalized difference between the timestamp of each activity in the partial trace
+                                 and the timestamp of the previous one
+    """
     # Raggruppo le righe con lo stesso caseID, i valori di timestamp e activity verranno raggruppati in liste
-    # Es:  CaseID: 1-364285768, Timestamp: ['2010-03-31T15:59:42+01:00', ... '2012-05-11T00:26:15+01:00'],
-    # Activity: ['Accepted - In Progress', ..., 'Completed - Closed'].
+    # Es:  CaseID: 1-364285768, Activity: ['Accepted - In Progress', ..., 'Completed - Closed'],
+    # Timestamp: ['2010-03-31T15:59:42+01:00', ... '2012-05-11T00:26:15+01:00'].
     # Il tutto sarà un nuovo dataframe che avrà come row index CaseID.
     grouped = dataset.groupby('CaseID').agg(lambda x: list(x))
 
@@ -56,36 +91,29 @@ def chain_dataset(dataset, chain_len, verbose, save_to_disk, filename, time_form
         timestamp_local_diff_list[:] = [x / max(timestamp_local_diff_list) if max(timestamp_local_diff_list) > 0 else 0
                                         for x in timestamp_local_diff_list]
 
-
-        """
-        # Divido la activity list in n liste, ognuna con una lunghezza di chain_len.
+        # Divido la activity list in n liste, ognuna con una lunghezza di window_size.
         # Aggiungo alla lista bpi13_dicts un dizionario per ogni catena ottenuta dal trace. Il dizionario è composto da:
         # "caseID" => l' id del trace
-        # "activity_chain": activity_list[i * chain_len: (i + 1) * chain_len] => la catena di chain_len attività
-        # "next_activity": activity_list[(i + 1) * chain_len] if (((i + 1) * chain_len)
-        #       < len(activity_list)) else "end") => l' attività successiva (se esiste),
-        #       oppure "end" (se mi trovo a fine lista)
-        # "timestamps" => differenza in minuti tra il timestamp delòle attività della catena e il timestamp della prima
-        #       attività eseguita nel trace a cui quella catena appartiene
-        for i in range((len(activity_list) + chain_len - 1) // chain_len):
-            dataset_dicts.append({
-                "caseID": caseID,
-                "activity_chain": activity_list[i * chain_len: (i + 1) * chain_len],
-                "next_activity": activity_list[(i + 1) * chain_len] if (((i + 1) * chain_len)
-                                                                        < len(activity_list)) else "end",
-                "global_timestamps": timestamp_diff_list[i * chain_len: (i + 1) * chain_len],
-                "local_timestamps": timestamp_local_diff_list[i * chain_len: (i + 1) * chain_len]
-            })
-        """
+        # "activity_chain": il trace pariale di dimensione window_size
+        # "next_activity": la prossima attività da eseguire o "end" se il trace è finito
+        # "global_timestamps" => differenza in minuti normalizzata tra ogni timestamp e il timestamp della prima
+        #                        activity del trace
+        # "local_timestamps" => differenza in minuti normalizzata tra ogni timestamp e il timestamp dell' activity
+        #                       precedente nel trace
+
         for i in range(len(activity_list)):
-            dataset_dicts.append({
-                "caseID": caseID,
-                "activity_chain": activity_list[i: (i + chain_len)],
-                "next_activity": activity_list[(i + chain_len)] if ((i + chain_len)
-                                                                        < len(activity_list)) else "end",
-                "global_timestamps": timestamp_diff_list[i: (i + chain_len)],
-                "local_timestamps": timestamp_local_diff_list[i: (i + chain_len)]
-            })
+            partial_trace = activity_list[i: (i + window_size)]
+
+            # Considero solo i trace parziali con almeno 2 attività
+            if partial_trace.__len__() > 1:
+                dataset_dicts.append({
+                    "caseID": caseID,
+                    "activity_chain": partial_trace,
+                    "next_activity": activity_list[(i + window_size)] if ((i + window_size)
+                                                                          < len(activity_list)) else "end",
+                    "global_timestamps": timestamp_diff_list[i: (i + window_size)],
+                    "local_timestamps": timestamp_local_diff_list[i: (i + window_size)]
+                })
 
     # Creo il dataframe da restituire in output
     dataset_chained = pd.DataFrame(dataset_dicts)
@@ -99,3 +127,81 @@ def chain_dataset(dataset, chain_len, verbose, save_to_disk, filename, time_form
 
     return dataset_chained
 
+
+def create_matrices(dataset, save_to_disk=False, filename="", consider_time_features=True):
+    """
+    This function creates the parallact matrices (both features and target matrix).
+
+    The features matrix takes into account the time relation between the activity in the chain.
+    It has a column for each transition between activities at a given time moment t.
+    i.e. given the two chains:
+     - 'Accepted - In Progress', 'Accepted - In Progress', 'Queued - Awaiting Assignment' => 'Accepted - In Progress'
+     - 'Accepted - In Progress', 'Queued - Awaiting Assignment', 'Accepted - In Progress' => 'Completed - Resolved'
+     There will be generated 5 columns:
+       - 1_Accepted_In_Progress => which represents the transition from t0 to t1 executing Accepted_In_Progress action
+       - 2_Accepted-In Progress => which represents the transition from t1 to t2 executing Accepted_In_Progress action
+       - 3_Queued_Awaiting_Assignment => which represents the transition from t2 to t3 executing
+                                        Queued_Awaiting_Assignment action
+       - 2_Queued_Awaiting Assignment => which represents the transition from t1 to t2 executing
+                                        Queued_Awaiting_Assignment action
+       - 3_Accepted_In Progress => which represents the transition from t2 to t3 executing Accepted_In Progress action
+    Considering the column order just described the two feature matrix will have two rows:
+      - 1 1 1 0 0
+      - 1 0 0 1 1
+
+    In a similar manner we compute the target matrix. It has a column for each of the possible next activity, and the
+    value of the column is 1 if that activity is executed, 0 otherwise.
+    If we consider the previous example chains we'll have two columns:
+      - Accepted-InProgress
+      - Completed-Resolved
+    Given this column order the target matrix will have two rows:
+     - 1 0
+     - 0 1
+
+    :param save_to_disk: if True both the features and the targets matrix will be stored into a csv
+    :param dataset: a pandas dataframe contains three columns: caseID", "activity_chain", "next_activity".
+    :param filename: the name of the file that will be written to disk
+    :param consider_time_features: if True the features extracted from timestamp will be considered
+
+    :return:
+      - features: a numpy array representing the features matrix just described
+      - targets: a numpy array representing the target matrix just described
+      - list(features_matrix.columns): a list of all the features name
+      - list(target_matrix.columns): a list of all the target name
+    """
+
+    # sfrutto una lista di dizionari per creare il dataframe contente la matrice delle feature.
+    # ogni dizionario presente nella lista avrà come chiave il nome della colonna
+    # (composto da posizione temporale + attività eseguita) e come valore 1.
+    features_dict_list = []
+    time_dict_list = []
+    target_dict_list = []
+    for _, row in dataset.iterrows():
+        feature_dict = {}
+        time_dict = {}
+        for time_instant, activity in enumerate(row[1]):
+            feature_dict[str(time_instant + 1) + "_" + str(activity).replace(" ", "")] = 1
+
+        if consider_time_features:
+            for time_instant, timestamp in enumerate(row[3]):
+                time_dict["time_diff_" + str(time_instant + 1)] = timestamp
+            for time_instant, timestamp in enumerate(row[4]):
+                time_dict["time_local_diff_" + str(time_instant + 1)] = timestamp
+
+        features_dict_list.append(feature_dict)
+        time_dict_list.append(time_dict)
+        target_dict_list.append({str(row[2]).replace(" ", ""): 1})
+
+    features_matrix = pd.DataFrame(features_dict_list).fillna(int(0))  # .astype(int)
+    time_matrix = pd.DataFrame(time_dict_list).fillna(int(0))
+    features_matrix = features_matrix.join(time_matrix)
+    features = features_matrix.to_numpy()
+
+    target_matrix = pd.DataFrame(target_dict_list).fillna(0)  # .astype(int)
+    targets = target_matrix.to_numpy()
+
+    if save_to_disk:
+        features_matrix.to_csv(fr"..\datasets\processed\{filename}_features_matrix.csv", index=False, header=True)
+        target_matrix.to_csv(fr"..\datasets\processed\{filename}_targets_matrix.csv", index=False, header=True)
+
+    return features, targets, list(features_matrix.columns), list(target_matrix.columns)
--- parallact/test.py
+++ parallact/test.py
@@ -1,4 +1,89 @@
 import parallact
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
 
-bpi12_dataset = parallact.load_generic_dataset("../datasets/vinc/bpi_12_w.csv", save_to_disk=True, verbose=True,
-                                                 filename="prova", time_format="%Y-%m-%d %H:%M:%S")
+import tensorflow as tf
+from tensorflow import keras
+
+
+def execute_deep(x_train, x_test, y_train, y_test):
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=1)  # 0.25x0.8=0.2
+
+    model = keras.Sequential([
+        keras.layers.Dense(units=np.shape(x_train)[1], activation='relu'),
+        keras.layers.Dense(units=(np.shape(x_train)[1]/2), activation='relu'),
+        keras.layers.Dense(units=np.shape(y_train)[1], activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss=tf.losses.CategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+    history = model.fit(
+        x_train, y_train,
+        epochs=10,
+        validation_data=(x_val, y_val),
+        verbose=True
+    )
+
+    predictions = model.predict(x_test)
+
+    discrete_pred = np.argmax(predictions, axis=1)
+    discrete_y_test = np.argmax(y_test, axis=1)
+    equality = tf.math.equal(discrete_pred, discrete_y_test)
+    accuracy = tf.math.reduce_mean(tf.cast(equality, tf.float32))
+
+    print(f"        train accuracy = {history.history['accuracy'][-1]}")
+    print(f"        test accuracy = {accuracy}")
+
+
+def execute_ml(x_train, x_test, y_train, y_test):
+    # model = OneVsRestClassifier(tree.DecisionTreeClassifier()).fit(x_train, y_train)
+    # model = OneVsRestClassifier(SVC(gamma=100, C=1000)).fit(x_train, y_train)
+    # predictions_train = model.predict(x_train)
+    # predictions_test = model.predict(x_test)
+
+    pca = PCA(n_components=3)
+
+    tree = OneVsRestClassifier(DecisionTreeClassifier())
+
+    pipe = Pipeline(steps=[('pca', pca), ('tree', tree)])
+    pipe = Pipeline(steps=[('tree', tree)])
+    pipe.fit(x_train, y_train)
+    predictions_train = pipe.predict(x_train)
+    predictions_test = pipe.predict(x_test)
+
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
+    print(f"        Train accuracy: {accuracy_score(y_train, predictions_train)}")
+    print(f"        Test accuracy: {accuracy_score(y_test, predictions_test)}")
+
+
+def bpi12():
+    print("bpi12")
+    bpi12_dataset = parallact.load_generic_dataset("../datasets/vinc/bpi_12_w.csv", save_to_disk=True, verbose=True,
+                                                 filename="bpi12", time_format="%Y-%m-%d %H:%M:%S")
+    features, targets, features_name, targets_name = parallact.create_matrices(bpi12_dataset, save_to_disk=True,
+                                                                             filename="bpi12")
+    print("Dataset loaded and preprocessed")
+
+    print("    bpi12 ml:")
+    x_train, x_test, y_train, y_test = train_test_split(features, targets, test_size=0.2, random_state=3)
+    execute_ml(x_train, x_test, y_train, y_test)
+
+    print("    bpi12 mlp:")
+    execute_deep(x_train, x_test, y_train, y_test)
+
+    print("")
+
+
+if __name__ == '__main__':
+    #bpi13()
+    bpi12()
+    #helpdesk()
+
+
+
+
